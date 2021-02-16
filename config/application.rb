4d898b26924fce026e5cf9c8bf0025ccaae72873
Lucas Expedito <lucasexp2013@gmail.com>
2021-02-16 13:17:42 -0300

    Adjustments

--- config/application.rb
+++ config/application.rb
@@ -4,16 +4,13 @@ require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-Bundler.require(:default, :assets, Rails.env)
-
+Bundler.require(*Rails.groups)
 
 module NewPlus
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.assets.enabled = true
-    config.assets.version = '1.0'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
